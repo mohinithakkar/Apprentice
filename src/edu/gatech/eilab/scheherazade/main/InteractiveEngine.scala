d14e3ffc96429f6dd9445cd928b67571ad743a3c
unknown <Albert@Glinda.cc.gt.atl.ga.us>
2012-09-01 21:51:51 -0400

    interactive storytelling engine with minor bugs

--- src/edu/gatech/eilab/scheherazade/main/InteractiveEngine.scala
+++ src/edu/gatech/eilab/scheherazade/main/InteractiveEngine.scala
@@ -4,6 +4,9 @@ import main.StoryGenerater._
 import utils.CSVProcessor
 import data._
 import graph._
+import utils.ReadTimeOut
+import java.io._
+import java.nio.channels.ClosedChannelException
 
 package main {
 
@@ -37,13 +40,28 @@ package main {
       execute(sources, ends, graph, me, optionals, desc)
     }
 
-    def execute(sources: List[Cluster], ends: List[Cluster], graph: Graph, me: List[MutualExcl], optionals: List[Cluster], desc:List[TextDescription]) {
+    def execute(sources: List[Cluster], ends: List[Cluster], graph: Graph, me: List[MutualExcl], optionals: List[Cluster], desc: List[TextDescription]) {
       var walk = Walk.fromInits(sources, graph, me, optionals)
       var step: Cluster = null
 
+      val actors = desc.map(_.actor).distinct
+
+      var actor = ""
+      while (actor == "") {
+        println("Choose an actor: ")
+        for (i <- 0 until actors.length) {
+          println((i + 1) + ". " + actors(i))
+        }
+
+        val actorNum = readLine().toInt - 1
+
+        if (actorNum < 0 || actorNum >= actors.length) println("Invalid choice")
+        else actor = actors(actorNum)
+      }
+
       do {
         var fringe = walk.fringe
-        var step = makeChoice(fringe, desc)
+        var step = makeChoice(fringe, desc, actor)
         walk = walk.nextFringe(step, me, optionals)
       } while (!ends.contains(step))
 
@@ -52,20 +70,97 @@ package main {
 
     }
 
-    def makeChoice(choices: List[Cluster], desc: List[TextDescription]): Cluster = {
-      println("Now you have the following choices: ")
-      for (i <- 0 until choices.length) {
-        val choice = choices(i)
-        val optionText = desc.find(text => text.name == choice.name).get.optionText
-        println(i + ". " + optionText)
+    def makeChoice(choices: List[Cluster], desc: List[TextDescription], actor: String): Cluster = {
+
+      val descripts = choices.map { c =>
+        val d = desc.find(_.name == c.name).get
+        (c, d)
       }
 
-      val n = readLine().trim.toInt
+      val humanChoices = descripts.filter(pair => pair._2.actor == actor)
+      val npcChoices = descripts filterNot (humanChoices contains)
+      var chosen: (Cluster, TextDescription) = null
+      var madeByHuman = true
 
-      val chosen = choices(n)
-      val result = desc.find(text => text.name == chosen.name).get.SelfExec
+      while (chosen == null) { // repeat until a choice is made
+
+        println("Now you have the following choices: ")
+        for (i <- 0 until humanChoices.length) {
+          val optionText = humanChoices(i)._2.optionText
+          println((i + 1) + ". " + optionText)
+        }
+        print("Your choice is: ")
+
+        var readText = ""
+        if (npcChoices.size > 0) {
+          // can make a npc choice. apply a timeout on input
+          var sleep = 0
+          while (readText == "" && sleep < 10) 
+          {
+            if (System.in.available() > 0)
+            {
+              val char = Array.ofDim[Char](3)
+              var i = 0
+              while(System.in.available() > 0 && i < 3)
+              {
+                char(i) = System.in.read().asInstanceOf[Char]
+                i += 1
+              }
+              readText = new String(char).substring(0, 1)              
+            }
+            else
+            {
+            	sleep += 1
+            	Thread.sleep(500) // sleep for 0.5 sec
+            }
+          }
+          /*
+          val in = ReadTimeOut.extract(System.in);
+          try {
+            if (!in.isInstanceOf[FileInputStream])
+              throw new RuntimeException(
+                "Could not extract a FileInputStream from STDIN.");
+
+            val ret = ReadTimeOut.maybeAvailable(in.asInstanceOf[FileInputStream], 5000);
+            val readText = {
+              if (ret == 0) ""
+              else ReadTimeOut.buf.asCharBuffer().subSequence(0, ret).toString
+            }
+          } catch {
+            case e: Exception =>
+              readText = ""
+          } finally {
+            in.close
+          }
+          */
+
+        } else {
+          readText = readLine()
+        }
+
+        if (readText == "") {
+
+          if (npcChoices.length > 0) {
+            // select a npc choice
+            val idx = math.floor(math.random * npcChoices.length).toInt
+            chosen = npcChoices(idx)
+            madeByHuman = false
+          }
+        } else {
+          val idx = readText.toInt - 1
+          if (idx < 0 || idx >= humanChoices.length)
+            println("Invalid Choice. Choose from 1 to " + humanChoices.length)
+          else {
+            chosen = humanChoices(idx)
+          }
+        }
+      }
+
+      val result = if (madeByHuman) chosen._2.SelfExec else chosen._2.otherExec
+      println()
       println(result)
-      chosen
+      println()
+      chosen._1
     }
 
     def readDescriptions(filename: String): List[TextDescription] = {
@@ -76,4 +171,100 @@ package main {
   }
 
   case class TextDescription(val name: String, val actor: String, val optionText: String, val SelfExec: String, val otherExec: String)
+  /*
+  object ReadMaybe {
+    
+    import java.io.FileInputStream;
+import java.io.FilterInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Field;
+import java.nio.ByteBuffer;
+import java.nio.channels.ClosedByInterruptException;
+
+	final buf = ByteBuffer.allocate(4096);
+
+	def readLine(timeout:Long):String = {
+			val in = extract(System.in);
+			if (!(in instanceof FileInputStream))
+				throw new RuntimeException(
+						"Could not extract a FileInputStream from STDIN.");
+
+				val ret = maybeAvailable(in.asInstanceOf[FileInputStream], timeout);
+				if (ret == 0)
+					return "";
+				else {
+					val result = buf.asCharBuffer().subSequence(0, ret).toString();
+					return result;
+				}
+	}
+
+	/*
+	 * unravels all layers of FilterInputStream wrappers to get to the core
+	 * InputStream
+	 */
+	private def extract(in:InputStream): InputStream = {
+
+	  var k = in
+		val f = classOf[FilterInputStream].getDeclaredField("in");
+		f.setAccessible(true);
+
+		while (k.isInstanceof[FilterInputStream])
+			k = f.get(k.asInstanceOf[FilterInputStream]).asInstanceOf[InputStream]
+
+		return in
+	}
+
+	/*
+	 * Returns the number of bytes which could be read from the stream, timing
+	 * out after the specified number of milliseconds. Returns 0 on timeout
+	 * (because no bytes could be read) and -1 for end of stream.
+	 */
+	private def maybeAvailable(in:FileInputStream, timeout:Long):Int =
+			 {
+
+		var dataReady = Array[Int].ofDim(1);
+		final IOException[] maybeException = { null };
+		final Thread reader = new Thread() {
+			public void run() {
+				try {
+					dataReady[0] = in.getChannel().read(buf);
+				} catch (ClosedByInterruptException e) {
+					System.err.println("Reader interrupted.");
+				} catch (IOException e) {
+					maybeException[0] = e;
+				}
+			}
+		};
+
+		Thread interruptor = new Thread() {
+			public void run() {
+				reader.interrupt();
+			}
+		};
+
+		reader.start();
+		for (;;) {
+
+			reader.join(timeout);
+			if (!reader.isAlive())
+				break;
+
+			interruptor.start();
+			interruptor.join(1000);
+			reader.join(1000);
+			if (!reader.isAlive())
+				break;
+
+			System.err.println("Fatal: Timeout during readline is not supported. Are you running this with Eclipse within an IDE? ");
+			System.exit(1);
+		}
+
+		if (maybeException[0] != null)
+			throw maybeException[0];
+
+		return dataReady[0];
+	}
+}
+*/
 }
